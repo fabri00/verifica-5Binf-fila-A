60063073f31cf0809f37c9ddc8aa58510754a76b
Rick <riccardoforteleoni@icloud.com>
2018-11-20 10:21:04 +0100

    Verifica Orsini 5binf Color Picker

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,27 +1,119 @@
 
-// TODO Inserire il package corretto
+// DONE Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package it.marconicivitavecchia.as2018_19.c5Binf.verifica;
 
 import javax.swing.*;
 
 public class Main extends JFrame {
 	
 	public Main() {
-		super("Formula 1 Passion");
+		super("Color Picker");
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
-		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		// DONE Inserire la size in accordo con il wireframe (valori approssimati)
+		super.setSize(400,600);
 		
-		// TODO Usare StringBuilder per costruire la stringa HTML
+		// DONE Usare StringBuilder per costruire la stringa HTML
+		StringBuilder sb = new StringBuilder();	
 		
-		// TODO Creare la stringa HTML per il wireframe
+		// DONE Creare la stringa HTML per il wireframe
+		sb.append("<html><body>");
+		sb.append("<h1>Color Picker</h1>");
 		
-		// TODO Convertire lo StringBuilder in String
+		// Create div for table
+		sb.append("<div style='display:table'>");
+
+		// create the first row
+		sb.append("<div style='display:table-row'>");
+
+		// create cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<b>Name</b>");
+		sb.append("</div>");
+		// create cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<b>Hex</b>");
+		sb.append("</div>");
+		// create cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<b>RGB</b>");
+		sb.append("</div>");
+
+		// close first row
+		sb.append("</div>");
 		
-		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		
+		// create the second row
+		sb.append("<div style='display:table-row'>");
+
+		// create cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("Persian Green");
+		sb.append("</div>");
+		// create cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("#00A693");
+		sb.append("</div>");
+		// create cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("0,166,147");
+		sb.append("</div>");
+
+		// close second row
+		sb.append("</div>");
+		
+		
+		// create the 3 row
+		sb.append("<div style='display:table-row'>");
+
+		// create cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("Venice Blue");
+		sb.append("</div>");
+		// create cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("#055989");
+		sb.append("</div>");
+		// create cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("5,89,137");
+		sb.append("</div>");
+
+		// close 3 row
+		sb.append("</div>");
+		
+		
+		// create the 4 row
+		sb.append("<div style='display:table-row'>");
+
+		// create cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("Pale Blue");
+		sb.append("</div>");
+		// create cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("#73D1E4");
+		sb.append("</div>");
+		// create cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("115,209,228");
+		sb.append("</div>");
+
+		// close 4 row
+		sb.append("</div>");
+		
+		
+		
+		
+		//close table
+		sb.append("</div>");
+
+		// DONE Convertire lo StringBuilder in String
+		String str = sb.toString();
+		
+		// DONE Assegnare correttamente la stringa create a JLabel
+		super.add(new JLabel(str));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
